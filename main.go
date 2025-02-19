6bc0fe053e0547fc45bb215ac527a63d54254dfd
tbk81 <dev@tbk81.com>
2025-02-19 14:41:44 -0800

    faq page

--- main.go
+++ main.go
@@ -15,6 +15,26 @@ func contactHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "<h1>Contact Page</h1><p>To get in touch, email me at <a href=\"mailto:dev@tbk81.com\">dev@tbk81.com</a>.")
 }
 
+func faqHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprint(w, `<h1>FAQ Page</h1>
+<ul>
+  <li>
+    <b>Is there a free version?</b>
+    Yes! We offer a free trial for 30 days on any paid plans.
+  </li>
+  <li>
+    <b>What are your support hours?</b>
+    We have support staff answering emails 24/7, though response
+    times may be a bit slower on weekends.
+  </li>
+  <li>
+    <b>How do I contact support?</b>
+    Email us - <a href="mailto:dev@tbk81.com">dev@tbk81.com</a>
+  </li>
+</ul>`)
+}
+
 // func errorHandler(w http.ResponseWriter, r *http.Request){
 // 	w.(http.StatusNotFound)
 // }
@@ -40,6 +60,8 @@ func (router Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		homeHandler(w, r)
 	case "/contact":
 		contactHandler(w, r)
+	case "/faq":
+		faqHandler(w, r)
 	default:
 		http.Error(w, "Page not found!", http.StatusNotFound)
 	}
